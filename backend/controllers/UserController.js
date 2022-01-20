02bd052542a6c6914cd1b0131c1f82e2326e18e6
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-01-19 21:26:43 -0300

    Validações de Usuario

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,39 @@
+const User = require('../models/User')
+
+module.exports = class UserController {
+
+    static async register(req, res) {
+
+        const { name, email, phone, password, confirmpassword } = req.body
+
+        //validation
+        if (!name) {
+            return res.status(422).json({ message: 'O nome é obrigatorio' })
+        }
+        if (!email) {
+            return res.status(422).json({ message: 'O email é obrigatorio' })
+        }
+        if (!phone) {
+            return res.status(422).json({ message: 'O Telefone é obrigatorio' })
+        }
+        if (!password) {
+            return res.status(422).json({ message: 'A senha é obrigatorio' })
+        }
+        if (!confirmpassword) {
+            return res.status(422).json({ message: 'A confirmação de senha é obrigatorio' })
+        }
+        if (password !== confirmpassword) {
+            return res.status(422).json({ message: 'As senhas devem ser iguais' })
+        }
+
+        //check User Exists
+
+        const userExists = await User.findOne({ email })
+        if (userExists) {
+            return res.status(422).json({ message: 'Por favor utilize outro email!' })
+        }
+
+
+    }
+
+}
