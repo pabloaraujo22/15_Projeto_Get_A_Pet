0794aff9cbc326e7f6417e2fa322bc475b6cabab
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-01-21 17:42:26 -0300

    Iniciando Rotas Pets

---
+++ backend/controllers/PetController.js
@@ -0,0 +1,9 @@
+const Pet = require('../models/Pet')
+
+
+module.exports = class PetController {
+    //create a pet
+    static async create(req, res) {
+        res.json({ message: 'Deu Certo' })
+    }
+}
