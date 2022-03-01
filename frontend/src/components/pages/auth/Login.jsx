9f9457e4bf45b864e60d88c1fc0b8049fddd6713
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-03-01 17:35:59 -0300

    Criando formulario de login

--- frontend/src/components/pages/auth/Login.jsx
+++ frontend/src/components/pages/auth/Login.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import Input from '../../form/Input';
+import styles from '../../form/Form.module.css';
 export default function Login(props) {
+    function handleChange(e) {}
     return (
-        <section>
-            <h1>Login</h1>
+        <section className={styles.form_container}>
+            <h1>Entrar:</h1>
+            <form>
+                <Input
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Digite o seu email"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Senha"
+                    type="password"
+                    name="password"
+                    placeholder="Digite a sua Senha"
+                    handleOnChange={handleChange}
+                />
+                <input type="submit" value="Entrar" />
+                <p>
+                    Não tem conta? <Link to="/register">Clique aqui</Link>
+                </p>
+            </form>
         </section>
     );
 }
