53756d3835f6331bd1aadfe52ac2a66d617e0582
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-03-02 19:27:45 -0300

    Terminando Formulario de Pet

--- frontend/src/components/form/PetForm.jsx
+++ frontend/src/components/form/PetForm.jsx
@@ -12,12 +12,46 @@ export default function PetForm(props) {
     const [preview, setPreview] = useState([]);
     const colors = ['Branco', 'Preto', 'Cinza', 'Mesclado'];
 
-    function onFileChange(e) {}
-    function handleChange(e) {}
-    function handleColor(e) {}
+    function onFileChange(e) {
+        setPreview(Array.from(e.target.files));
+        setPet({ ...pet, images: [...e.target.files] });
+    }
+    function handleChange(e) {
+        setPet({ ...pet, [e.target.name]: e.target.value });
+    }
+    function handleColor(e) {
+        setPet({
+            ...pet,
+            color: e.target.options[e.target.selectedIndex].text,
+        });
+    }
+
+    function submit(e) {
+        e.preventDefault();
+        console.log(pet);
+        // handleSubmit(pet)
+    }
 
     return (
-        <form className={formStyles.form_container}>
+        <form onSubmit={submit} className={formStyles.form_container}>
+            <div className={formStyles.preview_pet_images}>
+                {preview.length > 0
+                    ? preview.map((image, index) => (
+                          <img
+                              src={URL.createObjectURL(image)}
+                              alt={pet.name}
+                              key={`${pet.name}+${index}`}
+                          />
+                      ))
+                    : pet.images &&
+                      pet.images.map((image, index) => (
+                          <img
+                              src={`${process.env.REACT_APP_API}/images/pets/${image}`}
+                              alt={pet.name}
+                              key={`${pet.name}+${index}`}
+                          />
+                      ))}
+            </div>
             <Input
                 text="Imagens do Pet"
                 type="file"
